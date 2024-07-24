2a96fff955fca29fb3601d720c34ab696e4da31e
nikhilrajganta <nikhilraj.ganta@proclink.com>
2024-07-24 14:45:08 +0530

    code modified

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -8,8 +8,10 @@ import { Component, input, Input } from '@angular/core';
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-  @Input() Title = ""
-  @Input() imageURL = ""
-  @Input() description = ""
-  @Input() rating = ""
+  @Input() movie ={
+    "name": "Vikram",
+    "poster": "https://m.media-amazon.com/images/M/MV5BMmJhYTYxMGEtNjQ5NS00MWZiLWEwN2ItYjJmMWE2YTU1YWYxXkEyXkFqcGdeQXVyMTEzNzg0Mjkx._V1_.jpg",
+    "rating": "8.4",
+    "summary": "Members of a black ops team must track and eliminate a gang of masked murderers."
+  }
 }
