87a6dc8465fab623acb5276d37eff399c7a3ca21
nikhilrajganta <nikhilraj.ganta@proclink.com>
2024-07-24 12:20:51 +0530

    code0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,4 +15,11 @@ import { CardComponent } from './card/card.component';
 export class AppComponent {
   // title = 'Arjun';
   // profileUrl = "https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png";
+
+  users = [
+    {name: "Candace Hegmann", ImgaeUrl: 'https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/257.jpg'},
+    {name: "oanne Ryan", ImgaeUrl: 'https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/678.jpg'},
+    {name: "Christy Daugherty", ImgaeUrl: 'https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/80.jpg'},
+    {name: "Kellie Schamberger",ImgaeUrl: 'https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/311.jpg'},
+  ];
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -12,10 +12,4 @@ export class UserComponent {
 @Input() name = ""
 @Input() ImgaeUrl = ""
 
-users = [
-  {name: "Candace Hegmann", ImgaeUrl: 'https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/257.jpg'},
-  {name: "oanne Ryan", ImgaeUrl: 'https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/678.jpg'},
-  {name: "Christy Daugherty", ImgaeUrl: 'https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/80.jpg'},
-  {name: "Kellie Schamberger",ImgaeUrl: 'https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/311.jpg'},
-];
 }
