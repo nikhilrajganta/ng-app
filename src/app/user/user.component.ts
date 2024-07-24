ff1a1ed00938a55fa9928b77848e9785718011d4
nikhilrajganta <nikhilraj.ganta@proclink.com>
2024-07-24 12:13:40 +0530

    code

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,7 +7,15 @@ import { Component,Input } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
+
 export class UserComponent {
 @Input() name = ""
 @Input() ImgaeUrl = ""
+
+users = [
+  {name: "Candace Hegmann", ImgaeUrl: 'https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/257.jpg'},
+  {name: "oanne Ryan", ImgaeUrl: 'https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/678.jpg'},
+  {name: "Christy Daugherty", ImgaeUrl: 'https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/80.jpg'},
+  {name: "Kellie Schamberger",ImgaeUrl: 'https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/311.jpg'},
+];
 }
